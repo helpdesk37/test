5115b1ed1123d3bf71464edc2544d931df71550a
sai.sagar <sai.sagar@cgsl-java136.cgsl.com>
2024-05-12 23:01:08 +0530

    test commiting

---
+++ CharacterReversing/src/com/learning/checkAphanumrics.java
@@ -0,0 +1,9 @@
+package com.learning;
+
+public class checkAphanumrics {
+
+	public static void main(String[] args) {
+		String str = "ABCDabcd<>?.[]";
+		StringBuilder sb = new StringBuilder(str);
+		}
+}
